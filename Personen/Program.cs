b32f85beb93a41041cca20debef09cf51cfe9b6d
SabineSchropper <sabine.schropper@gmail.com>
2020-06-02 11:57:13 +0200

    first Programm in Special Track

---
+++ Personen/Program.cs
@@ -0,0 +1,43 @@
+﻿using Personen.models;
+using System;
+using System.Collections.Generic;
+
+namespace Personen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>();
+
+            string[] fileContent = System.IO.File.ReadAllLines("C:\\Users\\DCV\\C#\\Personen\\persons.txt");
+
+
+            for(int i = 0; i < fileContent.Length; i++)
+            {
+                string[] partArray = fileContent[i].Split(";");
+                string name = partArray[0];
+                int age = Int32.Parse(partArray[1]);
+                string city = partArray[2];
+                Person person = new Person(name,age,city);
+                persons.Add(person);
+            }
+            for (int i = 0; i < persons.Count; i++)
+            {
+                switch (persons[i].name)
+                {
+                    case "Helga" :persons[i].job = "Coder"; break;
+                    case "Hans": persons[i].job = "Coder"; break;
+                    default: persons[i].job = "Officer"; break;
+                }
+            }
+            for (int i = 0; i < persons.Count; i++)
+            {
+                fileContent[i] = persons[i].name + ";" + persons[i].age + ";" + persons[i].city + ";" + persons[i].job;
+            }
+            System.IO.File.WriteAllLines("C:\\Users\\DCV\\C#\\Personen\\persons1.txt", fileContent);
+           
+
+        }
+    }
+}
---
+++ Personen/models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Personen.models
+{
+    class Person
+    {
+        public string name;
+        public int age;
+        public string city;
+        public string job;
+
+        public Person(string name, int age, string city)
+        {
+            this.name = name;
+            this.age = age;
+            this.city = city;
+        }
+    }
+}
